6232ccffec690897d78f8ced9046eb21219da097
g1spellman <g1spellman@student.bridgew.edu>
2025-12-01 10:42:31 -0500

    Basic background complete. Details needed and parking lot out of range

--- Project 7, Gavin Spellman.py
+++ Project 7, Gavin Spellman.py
@@ -13,8 +13,19 @@ with dearpy.window(label='Housing Affordability', width=900, height=900):
                                color=comp151Colors.BLUE, fill=comp151Colors.BLUE)
         dearpy.draw_text((100, 25), "Walmart",
                           color=comp151Colors.YELLOW, size=15)
+        dearpy.draw_rectangle((400, 0), (500, 100),
+                              color=comp151Colors.BLACK, fill=comp151Colors.WHITE)
         #Road (Truckers road)
         dearpy.draw_rectangle((0, 100), (900, 300),
+                              color=comp151Colors.WHITE, fill=comp151Colors.BLACK)
+        #Grass (Wildlife)
+        dearpy.draw_rectangle((0, 300), (900, 500),
+                              color=comp151Colors.WHITE, fill=comp151Colors.GREEN)
+        #2nd Road
+        dearpy.draw_rectangle((0, 500), (900, 700),
+                              color=comp151Colors.WHITE, fill=comp151Colors.BLACK)
+        #Parking lot
+        dearpy.draw_rectangle((0, 700), (900, 900),
                               color=comp151Colors.WHITE, fill=comp151Colors.BLACK)
 
 
