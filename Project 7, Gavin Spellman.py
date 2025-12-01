0477d8271f5c76d309abb1561e6a384d13f3dc48
g1spellman <g1spellman@student.bridgew.edu>
2025-12-01 10:24:14 -0500

    starting creation of background elements

--- Project 7, Gavin Spellman.py
+++ Project 7, Gavin Spellman.py
@@ -4,6 +4,12 @@ import dearpygui.dearpygui as dearpy
 #Colors
 import comp151Colors
 #------------------------------------------------------
+dearpy.create_context()
+dearpy.create_viewport(title='Housing Affordability', width=900, height=900)
+with dearpy.window(label='Housing Affordability', width=900, height=900):
+    with dearpy.drawlist(width=900, height=900):
+        dearpy.draw_arrow((100, 0), (100, 700), color=comp151Colors.LIGHT_GREY,
+                          thickness=3)
 
 
 
