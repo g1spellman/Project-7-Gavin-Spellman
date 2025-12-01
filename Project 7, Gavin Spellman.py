7e3c00efda01afb87b856fd0d1c983bbbb8851fb
g1spellman <g1spellman@student.bridgew.edu>
2025-12-01 10:57:24 -0500

    added lines for parking lot, doors, and car placeholder

--- Project 7, Gavin Spellman.py
+++ Project 7, Gavin Spellman.py
@@ -5,8 +5,8 @@ import dearpygui.dearpygui as dearpy
 import comp151Colors
 #------------------------------------------------------
 dearpy.create_context()
-dearpy.create_viewport(title='Housing Affordability', width=900, height=900)
-with dearpy.window(label='Housing Affordability', width=900, height=900):
+dearpy.create_viewport(title='Get to the car!', width=900, height=1000)
+with dearpy.window(label='Get to the car!', width=900, height=1000):
     with dearpy.drawlist(width=900, height=900):
         #Store
         dearpy.draw_rectangle((0, 0), (900, 100),
@@ -27,6 +27,25 @@ with dearpy.window(label='Housing Affordability', width=900, height=900):
         #Parking lot
         dearpy.draw_rectangle((0, 700), (900, 900),
                               color=comp151Colors.WHITE, fill=comp151Colors.BLACK)
+        #CAR PLACEHOLDER (DELETE)
+        dearpy.draw_rectangle((400, 700), (500, 900),
+                              color=comp151Colors.BLACK, fill=comp151Colors.RED)
+        dearpy.draw_line((100, 700), (100, 900),
+                            color=comp151Colors.WHITE)
+        dearpy.draw_line((200, 700), (200, 900),
+                         color=comp151Colors.WHITE)
+        dearpy.draw_line((300, 700), (300, 900),
+                         color=comp151Colors.WHITE)
+        dearpy.draw_line((400, 700), (400, 900),
+                         color=comp151Colors.WHITE)
+        dearpy.draw_line((500, 700), (500, 900),
+                         color=comp151Colors.WHITE)
+        dearpy.draw_line((600, 700), (600, 900),
+                         color=comp151Colors.WHITE)
+        dearpy.draw_line((700, 700), (700, 900),
+                         color=comp151Colors.WHITE)
+        dearpy.draw_line((800, 700), (800, 900),
+                         color=comp151Colors.WHITE)
 
 
 
